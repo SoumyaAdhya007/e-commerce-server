1628d0c9f37fad78940127a281707d95f609b12e
Soumya Adhya <112754567+SoumyaAdhya007@users.noreply.github.com>
2023-02-12 20:18:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const cors = require('cors')
 const app=express();
 app.use(cors())
 app.use(express.json());
+app.get("/",(req,res)=>{
+    res.send("Welcome to my server")
+})
 app.use("/users",UserRouter)
 app.use("/admins",Adminrouter)
 // app.use(authenticate)
